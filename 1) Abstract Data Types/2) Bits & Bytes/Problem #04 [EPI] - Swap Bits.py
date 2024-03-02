a3eb7a6c26f7f1341f0d27b5899931f5b3d18d3d
Álvaro S <62604640+alvarosf07@users.noreply.github.com>
2024-03-02 13:53:44 +0100

    Update Problem #04 [EPI] - Swap Bits.py

--- 1) Abstract Data Types/2) Bits & Bytes/Problem #04 [EPI] - Swap Bits.py
+++ 1) Abstract Data Types/2) Bits & Bytes/Problem #04 [EPI] - Swap Bits.py
@@ -1,6 +1,6 @@
+# Problem extracted from the book: "Elements of Programming Interviews"
 # https://translucent504.github.io/programming-interviews/epi42/
 
-# Problem extracted from the book: "Elements of Programming Interviews"
 # A 64-bit integer can be viewed as an array of 64bits, with the bit at index 0 corresponding to the least significant bit (LSB), 
 # and the bit at index 63 corresponding to the most significant bit (MSB). 
 # Implement code that takes as input a 64-bit integer and swaps the bits at indices i and j. 
