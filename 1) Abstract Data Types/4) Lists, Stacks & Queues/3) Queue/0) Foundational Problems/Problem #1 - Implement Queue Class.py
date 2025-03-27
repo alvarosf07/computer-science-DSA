6b5e8758beb367b061536757b07986b3c4302c5d
Álvaro S. <62604640+alvarosf07@users.noreply.github.com>
2025-03-27 20:01:11 +0100

    Update and rename Problem #1 - Implement Queue Class to Problem #1 - Implement Queue Class.py

---
+++ 1) Abstract Data Types/4) Lists, Stacks & Queues/3) Queue/0) Foundational Problems/Problem #1 - Implement Queue Class.py
@@ -0,0 +1,34 @@
+
+# Solution #1 - Implementing Queue in Python
+class Queue:
+  def __init__(self):
+    self.queue = []
+
+  def enqueue(self,val):
+    self.queue.append(val)
+
+  def dequeue(self,val):
+    if self.queue:
+      self.queue.pop(0)
+    raise IndexError("Dequeue from an empty queue")
+
+  def size(self):
+    return len(self.queue)
+    
+
+# Solution #2 - Implementing Queue in Python using deque library
+from collections import deque
+class Queue:
+  def __init__(self):
+    self.queue = deque()
+
+  def enqueue(self,val):
+    self.queue.append(val)
+
+  def dequeue(self,val):
+    if self.queue:
+      self.queue.popleft() # queue library allows efficient pops from end of the queue
+    raise IndexError("Dequeue from an empty queue")
+
+  def size(self):
+    return len(self.queue)
