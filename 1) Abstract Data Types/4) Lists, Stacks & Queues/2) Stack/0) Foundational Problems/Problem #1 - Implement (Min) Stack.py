a3d8b7a3a23d86ebd5833fc31c95fd8ce0ac96e5
Álvaro S. <62604640+alvarosf07@users.noreply.github.com>
2025-03-27 19:07:07 +0100

    Update Problem #1 - Implement (Min) Stack.py

--- 1) Abstract Data Types/4) Lists, Stacks & Queues/2) Stack/0) Foundational Problems/Problem #1 - Implement (Min) Stack.py
+++ 1) Abstract Data Types/4) Lists, Stacks & Queues/2) Stack/0) Foundational Problems/Problem #1 - Implement (Min) Stack.py
@@ -1,6 +1,31 @@
 # Define Min Stack
 # https://leetcode.com/problems/min-stack/
 
+# Solution #1 - Two Stacks
+class MinStack:
+
+    def __init__(self):
+        self.stack = []
+        self.min_stack = []
+
+    def push(self, val: int) -> None:
+        self.stack.append(val)
+        if not self.min_stack or val <= self.min_stack[-1]: 
+            self.min_stack.append(val)
+        
+    def pop(self) -> None:
+        val = self.stack.pop()
+        if val == self.min_stack[-1]:
+            self.min_stack.pop()
+
+    def top(self) -> int:
+        return self.stack[-1]
+
+    def getMin(self) -> int:
+        return self.min_stack[-1]
+
+
+# Solution #2 - Single Stack
 class MinStack:
 
     def __init__(self):
